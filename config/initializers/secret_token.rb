6085459c249a672ba71ec15faaf81a8db96cda01
FlyingLeafe <flyingleafe@gmail.com>
2012-12-18 15:39:22 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PyatachokRails::Application.config.secret_token = '598186cfff1c6e2f3e1d95c1a3bbe259d082d359e150da87f3ab8fe55c6f2548aa9e8cfb723be2b012d9261ed626b8edb0a36b2744ac7aa7cfc9b9119140b47d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PyatachokRails::Application.config.session_store :cookie_store, key: '_pyatachok_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PyatachokRails::Application.config.session_store :active_record_store
